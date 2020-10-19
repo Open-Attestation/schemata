4a6b79ffaec61ee648257ac428faff01ac498dbd
dhemutton <38589870+dhemutton@users.noreply.github.com>
2020-10-19 14:18:13 +0800

    feat: add geekout schema and tests (#2)
    * feat: add geekout schema and tests
    
    * fix: git ignore
    
    * fix: schema json
    
    * fix: readme typos
    
    * fix: schema fix
    
    * fix: pr edits for date type
    
    * fix: include lodash dev-dep
    
    Co-authored-by: Matthea Loo <dhemutton@users.noreply.github.com>

--- src/sg/gov/tech/geekout/1.0/schema.test.ts
+++ src/sg/gov/tech/geekout/1.0/schema.test.ts
@@ -1,5 +1,192 @@
+import Ajv from "ajv";
+import { cloneDeep, omit } from "lodash";
+import schema from "./schema.json";
+import sampleDocJson from "./sample-document.json";
+
+const ajv = new Ajv({ allErrors: true });
+const validator = ajv.compile(schema);
 describe("schema", () => {
-  it("should work", () => {
-    expect(true).toBe(true);
+  it("should work with valid json", () => {
+    expect(validator(sampleDocJson)).toBe(true);
+  });
+
+  describe("recipient", () => {
+    it("should return array of errors without recipient", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "recipient");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: "",
+          schemaPath: "#/required",
+          params: { missingProperty: "recipient" },
+          message: "should have required property 'recipient'"
+        }
+      ]);
+    });
+    it("should return array of errors without recipient name", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "recipient.name");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".recipient",
+          schemaPath: "#/properties/recipient/required",
+          params: { missingProperty: "name" },
+          message: "should have required property 'name'"
+        }
+      ]);
+    });
+  });
+
+  describe("programme", () => {
+    it("should return array of errors without programme", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "programme");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: "",
+          schemaPath: "#/required",
+          params: { missingProperty: "programme" },
+          message: "should have required property 'programme'"
+        }
+      ]);
+    });
+    it("should return array of errors without programme name", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "programme.name");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".programme",
+          schemaPath: "#/properties/programme/required",
+          params: { missingProperty: "name" },
+          message: "should have required property 'name'"
+        }
+      ]);
+    });
+    it("should return array of errors without programme startDate", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "programme.startDate");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".programme",
+          schemaPath: "#/properties/programme/required",
+          params: { missingProperty: "startDate" },
+          message: "should have required property 'startDate'"
+        }
+      ]);
+    });
+    it("should return array of errors if programme startDate is not date format", () => {
+      const badDoc = cloneDeep(sampleDocJson);
+      badDoc.programme.startDate = "2-2-2";
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "format",
+          dataPath: ".programme.startDate",
+          schemaPath: "#/properties/programme/properties/startDate/format",
+          params: { format: "date" },
+          message: 'should match format "date"'
+        }
+      ]);
+    });
+    it("should return array of errors without programme endDate", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "programme.endDate");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".programme",
+          schemaPath: "#/properties/programme/required",
+          params: { missingProperty: "endDate" },
+          message: "should have required property 'endDate'"
+        }
+      ]);
+    });
+    it("should return array of errors if endDate is not date format", () => {
+      const badDoc = cloneDeep(sampleDocJson);
+      badDoc.programme.endDate = "2-2-2";
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "format",
+          dataPath: ".programme.endDate",
+          schemaPath: "#/properties/programme/properties/endDate/format",
+          params: { format: "date" },
+          message: 'should match format "date"'
+        }
+      ]);
+    });
+  });
+
+  describe("signatory", () => {
+    it("should return array of errors without signatory", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "signatory");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: "",
+          schemaPath: "#/required",
+          params: { missingProperty: "signatory" },
+          message: "should have required property 'signatory'"
+        }
+      ]);
+    });
+    it("should return array of errors without signatory name", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "signatory.name");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".signatory",
+          schemaPath: "#/properties/signatory/required",
+          params: { missingProperty: "name" },
+          message: "should have required property 'name'"
+        }
+      ]);
+    });
+    it("should return array of errors without signatory position", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "signatory.position");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".signatory",
+          schemaPath: "#/properties/signatory/required",
+          params: { missingProperty: "position" },
+          message: "should have required property 'position'"
+        }
+      ]);
+    });
+    it("should return array of errors without signatory organisation", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "signatory.organisation");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".signatory",
+          schemaPath: "#/properties/signatory/required",
+          params: { missingProperty: "organisation" },
+          message: "should have required property 'organisation'"
+        }
+      ]);
+    });
+    it("should return array of errors without signatory signature", () => {
+      const badDoc = omit(cloneDeep(sampleDocJson), "signatory.signature");
+      expect(validator(badDoc)).toBe(false);
+      expect(validator.errors).toStrictEqual([
+        {
+          keyword: "required",
+          dataPath: ".signatory",
+          schemaPath: "#/properties/signatory/required",
+          params: { missingProperty: "signature" },
+          message: "should have required property 'signature'"
+        }
+      ]);
+    });
   });
 });
