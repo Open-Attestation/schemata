98f4c3b6ba7a7b1a4af5a625add57c9a13c7db3b
Nebulis <nebounet@gmail.com>
2020-10-21 16:09:46 +0800

    Feat/combine schemas (#3)
    * fix: schema json
    
    fix: readme typos
    
    fix: schema fix
    
    chore: add node_modules and dist in .gitignore
    
    chore: ignore lint on generated files
    
    chore: update package lock file
    
    fix: pr edits for date type
    
    fix: include lodash dev-dep
    
    fix: tests and include oa schema
    
    feat: separate file to handle oa and geekout
    
    * fix: gitignore squash issue
    
    * feat: update postinstall for new combined schema
    
    * fix: lint issue
    
    * fix: remove extra oa type generation
    
    * fix: edit publish script
    
    * fix: shift axios to dev dep
    
    * fix: readme
    
    Co-authored-by: Matthea Loo <dhemutton@users.noreply.github.com>

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 1. Add a file named `schema.json` in the `src` directory. The file must contain the newly added JSON schema and must be placed under a reversed domain name structure. For instance if your schema is related to `tech.gov.sg`, then place the file in `src/sg/gov/tech/<name>/<version>/schema.json`. In that example `<name>` and `<version>` must be replaced by real values.
 
    - The schema file name must strictly be `schema.json`. It will be automatically published.
+
+   > _All json files in the src folder will be published._
+
    - The schema will be published at the URL following the structure of the `src` folder. For instance, if your schema is in `src/sg/gov/tech/<name>/<version>/schema.json` then it will be available from `https://domain/sg/gov/tech/<name>/<version>`.
    - Always use `1.0` version for new schemata.
 
@@ -15,4 +18,37 @@
    - Run `npm run postinstall` to generate the types.
    - Update `src/index.ts` to export the generated types. In order to prevent name collision, use the `import *` notation to import all the types related to your new schema under a specific name. Then re-export that name.
 
-> Feel free to refer to previously created schemata for help.
+> _Feel free to refer to previously created schemata for help._
+
+---
+
+## Including Open Attestation schema requirements into existing schema
+
+1. In the same folder as your `schema.json`, create a json file labelled `<name>-open-attestation.json` with the following attributes:
+
+```json
+{
+  "$id": "...",
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "allOf": [
+    {
+      "$ref": "https://schema.openattestation.com/2.0/schema.json"
+    },
+    {
+      "$ref": "..."
+    }
+  ]
+}
+```
+In the example above: 
+
+- `"$id"` field is the URL from which the schema will be available at. 
+
+- `"$ref"` field is a reference to the URL where your existing schema is published. The first `"$ref"` field refers to the URL where the Open Attestation schema is at.
+
+2. Add tests for your schema. 
+
+>_If you are uncertain of the structure of an Open Attestation document, you can refer to [this](https://openattestation.com/docs/verifiable-document/raw-document) and append your existing sample document with the required fields._
+
+
---
+++ src/sg/gov/tech/geekout/1.0/geekout-open-attestation.test.ts
@@ -0,0 +1,116 @@
+import Ajv from "ajv";
+import { cloneDeep, omit } from "lodash";
+import schema from "./geekout-open-attestation.json";
+import sampleDocJson from "./geekout-open-attestation-document.json";
+import axios from "axios";
+
+function loadSchema(uri: string) {
+  return axios.get(uri).then(res => {
+    return res.data;
+  });
+}
+const ajv = new Ajv({ allErrors: true, loadSchema: loadSchema });
+let validator: Ajv.ValidateFunction;
+
+describe("schema", () => {
+  beforeAll(async () => {
+    validator = await ajv.compileAsync(schema);
+  });
+  it("should work with valid json", () => {
+    expect(validator(sampleDocJson)).toBe(true);
+  });
+
+  //To test if geekout schema is correctly merged
+  it("should return array of errors without recipient name", () => {
+    const badDoc = omit(cloneDeep(sampleDocJson), "recipient.name");
+    expect(validator(badDoc)).toBe(false);
+    expect(validator.errors).toStrictEqual([
+      {
+        keyword: "required",
+        dataPath: ".recipient",
+        schemaPath:
+          "https://schemata.openattestation.com/sg/gov/tech/geekout/1.0/schema.json/properties/recipient/required",
+        params: { missingProperty: "name" },
+        message: "should have required property 'name'"
+      }
+    ]);
+  });
+
+  //To test if oa schema is correctly merged
+  it("should return array of errors without issuer name", () => {
+    const badDoc = omit(cloneDeep(sampleDocJson), "issuers[0].name");
+    expect(validator(badDoc)).toBe(false);
+    expect(validator.errors).toStrictEqual([
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/required",
+        params: { missingProperty: "name" },
+        message: "should have required property 'name'"
+      },
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/allOf/1/required",
+        params: { missingProperty: "tokenRegistry" },
+        message: "should have required property 'tokenRegistry'"
+      },
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/required",
+        params: { missingProperty: "name" },
+        message: "should have required property 'name'"
+      },
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/definitions/certificateStore/required",
+        params: { missingProperty: "name" },
+        message: "should have required property 'name'"
+      },
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/definitions/certificateStore/required",
+        params: { missingProperty: "certificateStore" },
+        message: "should have required property 'certificateStore'"
+      },
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/required",
+        params: { missingProperty: "name" },
+        message: "should have required property 'name'"
+      },
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/allOf/1/required",
+        params: { missingProperty: "revocationStore" },
+        message: "should have required property 'revocationStore'"
+      },
+      {
+        keyword: "required",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/required",
+        params: { missingProperty: "name" },
+        message: "should have required property 'name'"
+      },
+      {
+        keyword: "not",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/properties/issuers/items/oneOf/4/allOf/1/not",
+        params: {},
+        message: "should NOT be valid"
+      },
+      {
+        keyword: "oneOf",
+        dataPath: ".issuers[0]",
+        schemaPath: "#/properties/issuers/items/oneOf",
+        params: { passingSchemas: null },
+        message: "should match exactly one schema in oneOf"
+      }
+    ]);
+  });
+});
