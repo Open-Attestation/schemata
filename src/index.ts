0446b74e28ed5cb7a7ab72d5b4850d1bc536042e
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-06-28 15:43:33 +0800

    fix: resolve merge conflicts with master

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import * as notarise from "./__generated__/sg/gov/tech/notarise/1.0/schema";
 import * as pdtHealthcert from "./__generated__/sg/gov/moh/pdt-healthcert/1.0/schema";
 import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/vaccination-healthcert/1.0/schema";
 
-export { geekout, notarise, pdtHealthcert, vaccinationHealthcert};
+export { geekout, notarise, pdtHealthcert, vaccinationHealthcert };
