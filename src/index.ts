500992dcf70433a1df4611b2f7d90e5de800c6e4
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-07-01 12:55:49 +0800

    feat: reorganisation and create vaccination schema (#25)
    * refactor: cleanup unused samples
    
    * feat: create vaccination healthcert schema
    
    * refactor: remove unused files
    
    * fix: update render-able samples
    
    * fix: validFrom should be date-time
    
    * refactor: rename to pdt-healthcert
    
    * fix: post-install script
    
    * fix: export generated pdt and vaccination types
    
    * fix: rename url paths
    
    * refactor: notarised to endorsed
    
    * fix: resolve merge conflicts with master
    
    * fix: remove old healthcert path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as geekout from "./__generated__/sg/gov/tech/geekout/1.0/schema";
 import * as notarise from "./__generated__/sg/gov/tech/notarise/1.0/schema";
-import * as healthcert from "./__generated__/sg/gov/moh/healthcert/1.0/schema";
 import * as pdtHealthcert from "./__generated__/sg/gov/moh/pdt-healthcert/1.0/schema";
-import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/healthcert/1.0/schema";
+import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/vaccination-healthcert/1.0/schema";
 
-export { geekout, notarise, healthcert, pdtHealthcert, vaccinationHealthcert };
+export { geekout, notarise, pdtHealthcert, vaccinationHealthcert };
