67dc2729e70809b8c4dcd6ba365cf6d43267e673
ChenRen <siachenren@gmail.com>
2021-08-23 14:23:12 +0800

    fix: update test to cover all required properties

--- src/io/tradetrust/certificate-of-origin/1.0/schema.test.ts
+++ src/io/tradetrust/certificate-of-origin/1.0/schema.test.ts
@@ -11,6 +11,25 @@ describe("certificate of origin schema", () => {
     expect(validator(sampleDocument)).toBe(true);
   });
 
+  it("should fail when document does not have supplyChainConsignment", () => {
+    const document = omit(cloneDeep(sampleDocument), "supplyChainConsignment");
+
+    expect(validator(document)).toBe(false);
+    expect(validator.errors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "dataPath": "",
+          "keyword": "required",
+          "message": "should have required property 'supplyChainConsignment'",
+          "params": Object {
+            "missingProperty": "supplyChainConsignment",
+          },
+          "schemaPath": "#/required",
+        },
+      ]
+    `);
+  });
+
   it("should fail when document does not have exporter", () => {
     const document = omit(cloneDeep(sampleDocument), "supplyChainConsignment.exporter");
 
@@ -30,6 +49,48 @@ describe("certificate of origin schema", () => {
     `);
   });
 
+  it("should fail when document does not have exporter's iD, name, postalAddress", () => {
+    const document = {
+      ...sampleDocument,
+      supplyChainConsignment: {
+        ...sampleDocument.supplyChainConsignment,
+        exporter: {}
+      }
+    };
+    expect(validator(document)).toBe(false);
+    expect(validator.errors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "dataPath": ".supplyChainConsignment.exporter",
+          "keyword": "required",
+          "message": "should have required property 'iD'",
+          "params": Object {
+            "missingProperty": "iD",
+          },
+          "schemaPath": "#/definitions/ImporterExporter/required",
+        },
+        Object {
+          "dataPath": ".supplyChainConsignment.exporter",
+          "keyword": "required",
+          "message": "should have required property 'name'",
+          "params": Object {
+            "missingProperty": "name",
+          },
+          "schemaPath": "#/definitions/ImporterExporter/required",
+        },
+        Object {
+          "dataPath": ".supplyChainConsignment.exporter",
+          "keyword": "required",
+          "message": "should have required property 'postalAddress'",
+          "params": Object {
+            "missingProperty": "postalAddress",
+          },
+          "schemaPath": "#/definitions/ImporterExporter/required",
+        },
+      ]
+    `);
+  });
+
   it("should fail when document does not have importer", () => {
     const document = omit(cloneDeep(sampleDocument), "supplyChainConsignment.importer");
 
@@ -44,6 +105,48 @@ describe("certificate of origin schema", () => {
             "missingProperty": "importer",
           },
           "schemaPath": "#/properties/supplyChainConsignment/required",
+        },
+      ]
+    `);
+  });
+
+  it("should fail when document does not have importer's iD, name, postalAddress", () => {
+    const document = {
+      ...sampleDocument,
+      supplyChainConsignment: {
+        ...sampleDocument.supplyChainConsignment,
+        importer: {}
+      }
+    };
+    expect(validator(document)).toBe(false);
+    expect(validator.errors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "dataPath": ".supplyChainConsignment.importer",
+          "keyword": "required",
+          "message": "should have required property 'iD'",
+          "params": Object {
+            "missingProperty": "iD",
+          },
+          "schemaPath": "#/definitions/ImporterExporter/required",
+        },
+        Object {
+          "dataPath": ".supplyChainConsignment.importer",
+          "keyword": "required",
+          "message": "should have required property 'name'",
+          "params": Object {
+            "missingProperty": "name",
+          },
+          "schemaPath": "#/definitions/ImporterExporter/required",
+        },
+        Object {
+          "dataPath": ".supplyChainConsignment.importer",
+          "keyword": "required",
+          "message": "should have required property 'postalAddress'",
+          "params": Object {
+            "missingProperty": "postalAddress",
+          },
+          "schemaPath": "#/definitions/ImporterExporter/required",
         },
       ]
     `);
