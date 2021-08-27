41cb21cecba3606b4508a43e97ddc6f08e0803f5
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-08-27 13:02:07 +0800

    refactor: improve code readability

--- scripts/lite-fhir-schema.js
+++ scripts/lite-fhir-schema.js
@@ -6,21 +6,20 @@ const filename = "src/sg/gov/moh/fhir/4.0.1/lite-schema.json";
 
 const resources = ["Bundle", "Device", "Observation", "Organization", "Patient", "Practitioner", "Specimen"];
 const resourceQueue = [...resources];
-const hasFetched = new Set();
+const hasFetched = new Set(["ResourceList"]); // Skip ResourceList
 
-const liteSchema = {
+const schemaHeader = {
   $schema: "http://json-schema.org/draft-07/schema#",
   $id: "https://schemata.openattestation.com/sg/gov/moh/fhir/4.0.1/lite-schema.json#",
   // id: "http://hl7.org/fhir/json-schema/4.0",
   title: "Lite FHIR schema for Notarise.gov.sg HealthCerts",
   description: "see http://hl7.org/fhir/json.html#schema for information about the FHIR Json Schemas",
-  $ref: "#/definitions/Bundle",
-  definitions: {}
+  $ref: "#/definitions/Bundle"
 };
 
 /** ====== Helper Functions ====== **/
 
-function stringToRef(ref = "") {
+function sanitiseRef(ref = "") {
   return ref.replace("#/definitions/", "");
 }
 
@@ -30,7 +29,7 @@ function findUniqueNestedReferences(obj = {}, results = []) {
 
   for (const [key, value] of Object.entries(obj)) {
     if (key === searchKey && typeof value !== "object") {
-      references.push(stringToRef(value));
+      references.push(sanitiseRef(value));
     } else if (typeof value === "object") {
       findUniqueNestedReferences(value, references);
     }
@@ -40,22 +39,23 @@ function findUniqueNestedReferences(obj = {}, results = []) {
 }
 
 /** ====== Main Script ====== **/
+const definitionEntries = [];
 
 while (resourceQueue.length > 0) {
   const resource = resourceQueue.shift();
 
   // 1. Extract resource definition
-  liteSchema.definitions[resource] = fhir.definitions[resource];
+  const definition = fhir.definitions[resource];
+  const properties = definition.properties
+    ? Object.keys(definition.properties)
+        // Filter out properties that start with "_" (since they are not needed)
+        .filter(key => !key.startsWith("_"))
+        .reduce((obj, key) => ({ ...obj, [key]: definition.properties[key] }), {})
+    : undefined;
+  definitionEntries.push([resource, { ...definition, properties }]);
 
-  // 2. Remove properties that start with "_" (since they are not needed)
-  Object.keys(liteSchema.definitions[resource].properties || {}).forEach(key => {
-    if (key.startsWith("_")) {
-      delete liteSchema.definitions[resource].properties[key];
-    }
-  });
-
-  // 3. Find nested references
-  const nestedReferences = findUniqueNestedReferences(liteSchema.definitions[resource].properties).filter(
+  // 2. Find nested references
+  const nestedReferences = findUniqueNestedReferences(definition.properties).filter(
     // Filter out definitions that have already been fetched
     r => !hasFetched.has(r)
   );
@@ -63,32 +63,29 @@ while (resourceQueue.length > 0) {
   hasFetched.add(...nestedReferences, resource);
 }
 
-// 4. Custom definitions.ResourceList: To limit the type of resources that can exist in Bundle_Entry and .contained
-const resourceListLength = liteSchema.definitions.ResourceList.oneOf.length;
-for (let i = 0; i < resourceListLength; i++) {
-  const item = liteSchema.definitions.ResourceList.oneOf.shift();
-  const ref = stringToRef(item.$ref);
-
-  if (resources.includes(ref)) {
-    liteSchema.definitions.ResourceList.oneOf.push(item);
-  }
-}
+// 3. Custom ResourceList: To limit the type of resources that can exist in Bundle_Entry and .contained
+const resourceList = {
+  oneOf: resources
+    .map(r => ({
+      $ref: `#/definitions/${r}`
+    }))
+    .sort()
+};
 
 /** ====== Export! ====== **/
 
-const sortedLiteschema = {
-  ...liteSchema,
-  definitions: Object.fromEntries(
-    // Sort definitions alphabetically
-    Object.entries(liteSchema.definitions).sort(([a], [b]) => {
-      // Push "ResourceList" definitions to the top
-      if (a === "ResourceList") return -1;
-      else if (b === "ResourceList") return 1;
-      else return a < b ? -1 : a > b ? 1 : 0;
-    })
-  )
+const liteSchema = {
+  ...schemaHeader,
+  definitions: {
+    ResourceList: resourceList,
+    ...Object.fromEntries(
+      definitionEntries
+        // Sort definitions alphabetically
+        .sort(([a], [b]) => (a < b ? -1 : a > b ? 1 : 0))
+    )
+  }
 };
-fs.writeFile(filename, JSON.stringify(sortedLiteschema, null, 2), err => {
+fs.writeFile(filename, JSON.stringify(liteSchema, null, 2), err => {
   if (err) console.error(err);
   else console.log(`Exported to ${filename} with ${Object.keys(liteSchema.definitions).length} definitions.`);
 });
