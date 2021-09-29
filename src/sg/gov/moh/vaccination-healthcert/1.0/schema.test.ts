ad1781e063278b781fcc5052a29fee9d09928742
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-09-29 10:56:25 +0800

    fix: update pdt and vaccination samples (#53)
    * fix: update pdt and vaccination samples
    
    * fix: update vaccination schema and tests

--- src/sg/gov/moh/vaccination-healthcert/1.0/schema.test.ts
+++ src/sg/gov/moh/vaccination-healthcert/1.0/schema.test.ts
@@ -1,11 +1,21 @@
 import Ajv from "ajv";
+import axios from "axios";
 import schema from "./schema.json";
 import vaccineHealthCert from "./interim-vaccination-healthcert-unwrapped.json";
 
-const ajv = new Ajv({ allErrors: true });
-const validator = ajv.compile(schema);
+function loadSchema(uri: string) {
+  return axios.get(uri).then(res => {
+    return res.data;
+  });
+}
+const ajv = new Ajv({ allErrors: true, loadSchema });
+let validator: Ajv.ValidateFunction;
 
 describe("interim vaccination healthcert schema", () => {
+  beforeAll(async () => {
+    validator = await ajv.compileAsync(schema);
+  });
+
   it("should work with valid vaccine healthcert", () => {
     expect(validator(vaccineHealthCert)).toBe(true);
   });
