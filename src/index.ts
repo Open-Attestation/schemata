b5f56c33c7bf9f84d7e21f9c72f56f4cc9003b46
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-01-11 13:47:58 +0800

    chore: remove pdt v1.0 schema

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as geekout from "./__generated__/sg/gov/tech/geekout/1.0/schema";
 import * as notarise from "./__generated__/sg/gov/tech/notarise/1.0/schema";
-import * as pdtHealthCertV1 from "./__generated__/sg/gov/moh/pdt-healthcert/1.0/schema";
 import * as pdtHealthCertV2 from "./__generated__/sg/gov/moh/pdt-healthcert/2.0/schema";
 import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/vaccination-healthcert/1.0/schema";
 
-export { geekout, notarise, pdtHealthCertV1, pdtHealthCertV2, vaccinationHealthcert };
+export { geekout, notarise, pdtHealthCertV2, vaccinationHealthcert };
