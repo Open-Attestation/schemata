2a8b920eaabdab58b9295ae8e354c4c2dddb1387
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-03-14 17:15:01 +0800

    feat: export PDT HealthCert schemas

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as geekout from "./__generated__/sg/gov/tech/geekout/1.0/schema";
 import * as notarise from "./__generated__/sg/gov/tech/notarise/1.0/schema";
 import * as pdtHealthCertV2 from "./__generated__/sg/gov/moh/pdt-healthcert/2.0/schema";
 import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/vaccination-healthcert/1.0/schema";
+import * as pdtHealthCertV2Schemas from "./sg/gov/moh/pdt-healthcert/2.0";
 
-export { geekout, notarise, pdtHealthCertV2, vaccinationHealthcert };
+export { geekout, notarise, pdtHealthCertV2, pdtHealthCertV2Schemas, vaccinationHealthcert };
