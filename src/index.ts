a9d637e817c1340dd4911c69fe12718f5c2235f5
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-03-16 10:59:19 +0800

    feat: export PDT HealthCert v2.0 schemas (#69)
    * feat: export PDT HealthCert schemas
    
    * feat: export fhir schema

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import * as notarise from "./__generated__/sg/gov/tech/notarise/1.0/schema";
 import * as pdtHealthCertV2 from "./__generated__/sg/gov/moh/pdt-healthcert/2.0/schema";
 import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/vaccination-healthcert/1.0/schema";
 
-export { geekout, notarise, pdtHealthCertV2, vaccinationHealthcert };
+import * as pdtHealthCertV2Schema from "./sg/gov/moh/pdt-healthcert/2.0";
+import * as fhirSchema from "./sg/gov/moh/fhir/4.0.1";
+
+export { geekout, notarise, pdtHealthCertV2, vaccinationHealthcert, pdtHealthCertV2Schema, fhirSchema };
---
+++ src/sg/gov/moh/pdt-healthcert/2.0/index.ts
@@ -0,0 +1,5 @@
+import schema from "./schema.json";
+import clinicProviderSchema from "./clinic-provider-schema.json";
+import endorsedSchema from "./endorsed-schema.json";
+
+export { schema, clinicProviderSchema, endorsedSchema };
