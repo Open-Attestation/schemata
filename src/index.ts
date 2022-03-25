bbffa6cce15f7c4118d7d42f19abb477d10ed94e
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-03-25 14:24:01 +0800

    chore: recovery healthcert schema

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import * as pdtHealthCertV2 from "./__generated__/sg/gov/moh/pdt-healthcert/2.0/
 import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/vaccination-healthcert/1.0/schema";
 
 import * as pdtHealthCertV2Schema from "./sg/gov/moh/pdt-healthcert/2.0";
+import * as recHealthCertV2Schema from "./sg/gov/moh/recovery-healthcert/2.0";
 import * as fhirSchema from "./sg/gov/moh/fhir/4.0.1";
 
-export { geekout, notarise, pdtHealthCertV2, vaccinationHealthcert, pdtHealthCertV2Schema, fhirSchema };
+export {
+  geekout,
+  notarise,
+  pdtHealthCertV2,
+  vaccinationHealthcert,
+  pdtHealthCertV2Schema,
+  recHealthCertV2Schema,
+  fhirSchema
+};
