67997c7861d5d2f9c5d784fab8f0d817a768ef78
Kyle Huang Junyuan <kyle@hjunyuan.com>
2022-03-29 16:20:09 +0800

    fix: export recovery healthcert types (#74)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as geekout from "./__generated__/sg/gov/tech/geekout/1.0/schema";
 import * as notarise from "./__generated__/sg/gov/tech/notarise/1.0/schema";
 import * as pdtHealthCertV2 from "./__generated__/sg/gov/moh/pdt-healthcert/2.0/schema";
+import * as recHealthCertV2 from "./__generated__/sg/gov/moh/recovery-healthcert/2.0/schema";
 import * as vaccinationHealthcert from "./__generated__/sg/gov/moh/vaccination-healthcert/1.0/schema";
 
 import * as pdtHealthCertV2Schema from "./sg/gov/moh/pdt-healthcert/2.0";
@@ -11,6 +12,7 @@ export {
   geekout,
   notarise,
   pdtHealthCertV2,
+  recHealthCertV2,
   vaccinationHealthcert,
   pdtHealthCertV2Schema,
   recHealthCertV2Schema,
