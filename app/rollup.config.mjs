571842cca454636ef67c1f14e18b3ebda846fe72
tngzixiang <tng_zi_xiang@tech.gov.sg>
2024-04-12 11:39:14 +0800

    chore: revert to use dedupe

--- app/rollup.config.mjs
+++ app/rollup.config.mjs
@@ -72,8 +72,7 @@ export default {
     // https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       browser: true,
-      exportConditions: ["svelte"],
-      extensions: [".svelte"],
+      dedupe: ["svelte"],
     }),
     commonjs(),
     typescript({
