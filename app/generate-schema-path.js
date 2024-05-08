85ec50933dcf7af5a977f2b06e2b9674981de9cc
tngzixiang <tng_zi_xiang@tech.gov.sg>
2024-05-08 10:13:26 +0800

    chore: exclude healthcerts samples

--- app/generate-schema-path.js
+++ app/generate-schema-path.js
@@ -3,13 +3,28 @@ const fs = require("fs");
 const path = require("path");
 
 const root = path.resolve(__dirname, "..", "src");
-const excludedFiles = ["draft-06-schema.json"];
+const excludedFiles = [
+  "draft-06-schema.json",
+  "clinic-provider-document.json",
+  "clinic-provider-schema.json",
+  "clinic-provider-wrapped.json",
+  "endorsed-document.json",
+  "endorsed-schema.json",
+  "endorsed-wrapped.json",
+  "sample-document.json",
+  "interim-vaccination-healthcert-unwrapped.json",
+  "interim-vaccination-healthcert-wrapped.json",
+  "notarise-open-attestation-document.json",
+  "notarise-open-attestation-schema.json",
+  "sample-document-pdt.json",
+  "sample-document-vac.json",
+];
 
 const schemataPath = [];
-const run = currentPath => {
+const run = (currentPath) => {
   const matches = [];
   const currentDirectory = path.resolve(root, currentPath);
-  const files = fs.readdirSync(currentDirectory).filter(name => !(name === "__generated__" || name === "__schema__"));
+  const files = fs.readdirSync(currentDirectory).filter((name) => !(name === "__generated__" || name === "__schema__"));
   for (const file of files) {
     const stats = fs.statSync(path.resolve(currentDirectory, file));
     if (stats.isDirectory()) {
